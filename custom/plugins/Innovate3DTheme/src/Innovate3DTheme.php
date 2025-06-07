16a11a7c2b33c9f6eb57b2ccb19cf3d5c3422879
EinsPommes <jannikkugler2006@gmail.com>
2025-06-07 15:42:40 +0200

    Update theme configuration and plugin class for proper theme registration

--- custom/plugins/Innovate3DTheme/src/Innovate3DTheme.php
+++ custom/plugins/Innovate3DTheme/src/Innovate3DTheme.php
@@ -3,10 +3,32 @@
 namespace Innovate3DTheme;
 
 use Shopware\Core\Framework\Plugin;
-use Shopware\Storefront\Framework\ThemeInterface;
+use Shopware\Core\Framework\Plugin\Context\InstallContext;
+use Shopware\Core\Framework\Plugin\Context\UninstallContext;
+use Shopware\Storefront\Theme\ThemeLifecycleService;
 
-class Innovate3DTheme extends Plugin implements ThemeInterface
+class Innovate3DTheme extends Plugin
 {
+    public function install(InstallContext $installContext): void
+    {
+        parent::install($installContext);
+        
+        $themeLifecycleService = $this->container->get(ThemeLifecycleService::class);
+        $themeLifecycleService->refreshThemes($installContext->getContext());
+    }
+
+    public function uninstall(UninstallContext $uninstallContext): void
+    {
+        parent::uninstall($uninstallContext);
+        
+        if ($uninstallContext->keepUserData()) {
+            return;
+        }
+        
+        $themeLifecycleService = $this->container->get(ThemeLifecycleService::class);
+        $themeLifecycleService->refreshThemes($uninstallContext->getContext());
+    }
+
     public function getThemeConfigPath(): string
     {
         return 'theme.json';
